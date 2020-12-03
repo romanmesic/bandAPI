582be2f8a19537b7bf3ffe04acb27c99932134a6
Roman Mesic <mesic.roman@gmail.com>
2020-12-03 18:33:30 +0100

    idk

---
+++ BandAPI/Controllers/BandsController.cs
@@ -0,0 +1,38 @@
+﻿using BandAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Controllers
+{
+  
+    
+    [ApiController]
+    [Route("api/bands")]
+    public class BandsController:ControllerBase
+    {
+
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandsController(IBandAlbumRepository bandAlbumRepository)
+        {
+
+            _bandAlbumRepository = bandAlbumRepository ?? 
+                throw new ArgumentNullException(nameof (bandAlbumRepository));
+
+        }
+
+        
+        [HttpGet]
+        public IActionResult GetBands()
+        {
+            var bandsFromRepo = _bandAlbumRepository.GetBands();
+            return new JsonResult(bandsFromRepo);
+        
+        }
+
+
+    }
+}
